deb7b74153733e8776082359c887fd8d9eb800a4
Arturo <art_ag7@hotmail.com>
2019-12-04 17:50:20 -0400

    ATDD Configurar Terreno

--- app.rb
+++ app.rb
@@ -1,2 +1,12 @@
 require 'sinatra'
 
+get '/configuracion' do
+    erb :configurar
+end
+
+post '/configuracion' do
+    @ancho_terreno = params[:ancho]
+    @alto_terreno = params[:alto]
+
+    erb :configurar
+end
