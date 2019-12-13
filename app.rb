be7301a2908c3e38fe49b2d27764815273a672a8
johancarlos <johancarlos.bo@gmail.com>
2019-12-13 07:31:09 -0400

    ATDD Bievenida terminada

--- app.rb
+++ app.rb
@@ -9,11 +9,19 @@ def initialize()
     @terreno = Terreno.new
     @vehiculo = Vehiculo.new
     @dashDot = DashDot.new
+    @nro_vehiculo_lista = 1
+    @ancho_terreno = 0
+    @alto_terreno = 0
+    @dashDot.agregarTerreno(@terreno)
 end
 
 get '/' do
     erb :bienvenida
 end
+
+# post '/' do
+#     erb :bienvenida
+# end
 
 get '/configuracion' do
     erb :configurar
@@ -24,24 +32,28 @@ post '/configuracion' do
     @alto_terreno = params[:alto].to_i
     @configuracion_terreno = @terreno.configurarTerreno(@ancho_terreno, @alto_terreno)
     @vehiculo = Vehiculo.new
+    @secuencia = Secuencia.new
 
     @x_vehiculo = params[:x].to_i
     @y_vehiculo = params[:y].to_i
     @orientacion = params[:orientacion]
     @configuracion_orientacion = @vehiculo.configurarOrientacion(@orientacion)
 
-    @dashDot.agregarTerreno(@terreno)
+    # @dashDot.agregarTerreno(@terreno)
     @dashDot.agregarVehiculo(@vehiculo)
     @configuracion_ubicacion = @dashDot.configurarUbicacionVehiculo(@vehiculo, @x_vehiculo, @y_vehiculo)
 
     @movimientos_ingresados = params[:secuencia]
     @secuencia_configurada = @secuencia.configurar_secuencia(@movimientos_ingresados)
 
-    @vehiculo.agregarSecuencia(@secuencia)
+    # @vehiculo.agregarSecuencia(@secuencia)
+    @dashDot.asignarSecuencia(@vehiculo, @secuencia)
     erb :configurar
 end
 
 post '/simulacion' do
+    @ancho_configurado = @dashDot.terreno.ancho
+    @alto_configurado = @dashDot.terreno.alto
     @dashDot.incrementarTam()
     @nro_vehiculo = 1
     #@dashDot.agregarVehiculo(@vehiculo)
